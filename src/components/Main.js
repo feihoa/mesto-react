2c80bb07d23a96782026ca005cdaa7b488b5117e
Maria Terenteva <rtillkriyt@gmail.com>
2021-01-21 22:02:16 +0300

    getting user data from the server

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,41 @@
 import React from 'react';
-
+import consts from './consts';
+import Api from './Api';
 
 class Main extends React.Component {
     constructor(props) {
         super(props);
+ 
+
+        this.state = {
+            userName: "Имя",
+            userDescription:  "Описание",
+            userAvatar: '../images/avatar.jpg',
         };
+        };
+
+        componentDidMount() {
+            consts.api.getUserInfo().then(data => { console.log(data)
+                this.setState({ 
+                    userName: data.name,
+                    userDescription: data.about,
+                    userAvatar: data.avatar,
+                });
+
+            });
+
+        }
+
     
 render() {
 return (
     <>    
     <div id='profileRoot' className="profile root__section">
         <div className="user-info">
-            <div id = "userInfoPic" className="user-info__photo" onMouseUp={this.props.onEditAvatar}></div>
+            <div id = "userInfoPic" className="user-info__photo" style={{ backgroundImage: 'url(' + this.state.userAvatar + ')', cursor: "pointer"}}  onMouseUp={this.props.onEditAvatar}></div>
             <div id="userInfoData" className="user-info__data">
-            <h1 id = "userInfoName" className="user-info__name">Имя</h1>
-            <p id = "userInfoJob" className="user-info__job">Профессия</p>
+            <h1 id = "userInfoName" className="user-info__name">{this.state.userName}</h1>
+            <p id = "userInfoJob" className="user-info__job">{this.state.userDescription}</p>
             <button id="userInfoEditButton" className="button user-info-edit__button" onMouseUp={this.props.onEditProfile}>Edit</button>
             </div>
             <button id="userInfoButton" className="button user-info__button" onMouseUp={this.props.onAddPlace}>+</button>
---
+++ src/components/consts.js
@@ -0,0 +1,15 @@
+import Api from './Api';
+
+const consts =  {
+
+    api : new Api({
+      baseUrl: 'https://nomoreparties.co/cohort9',
+      headers: {
+        authorization: '548c5797-a590-40d0-8f9e-48d758ca9ae7',
+        'Content-Type': 'application/json'
+      }
+    }),
+
+}
+
+export default consts;
